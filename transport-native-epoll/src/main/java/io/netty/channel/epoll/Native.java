506e65ab21475d6234e101e72d35a7b7f620d6ca
Norman Maurer <nmaurer@redhat.com>
2014-02-12 08:02:22 +0100

    Fix checkstyle

--- transport-native-epoll/src/main/java/io/netty/channel/epoll/Native.java
+++ transport-native-epoll/src/main/java/io/netty/channel/epoll/Native.java
@@ -34,7 +34,6 @@ final class Native {
     private static final byte[] IPV4_MAPPED_IPV6_PREFIX = {
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, (byte) 0xff, (byte) 0xff };
 
-
     private static final Library LIBRARY = new Library("native", Native.class);
     static {
         // Load the library
